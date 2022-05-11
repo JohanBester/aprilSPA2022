8316b462af2da50769b5592e96ee3b275da471c3
mjwebb1993 <mjwebb1993@yahoo.com>
2022-05-11 17:46:06 -0500

    test successful

--- components/views/Home.js
+++ components/views/Home.js
@@ -3,6 +3,6 @@ import html from "html-literal";
 export default () => html`
   <section id="jumbotron">
     <h2>SavvyCoders JavaScript Fullstack Bootcamp</h2>
-    <a href="index.html">"Test" "Button"</a>
+    <a href="index.html">"Call to Action" "Button"</a>
   </section>
 `;
