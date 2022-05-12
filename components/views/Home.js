45985d3e54636a467469f28cbd8c4947c4f4f806
Johan Bester <johan.bester@yahoo.com>
2022-05-12 07:41:26 -0500

    Update action button

--- components/views/Home.js
+++ components/views/Home.js
@@ -3,7 +3,7 @@ import html from "html-literal";
 export default () => html`
   <section id="jumbotron">
     <h2>SavvyCoders JavaScript Fullstack Bootcamp</h2>
-    <a href="index.html" onClick="alert('Hello! You clicked the Button!')"
+    <a href="" onClick="alert('Hello! You clicked the Button!')"
       >"Call to Action" "Button"</a
     >
   </section>
