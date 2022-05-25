b1e308cb7bbca2576b6ca182ae969a6c95b00ffd
matsinet <matsinet@gmail.com>
2022-05-25 08:42:05 -0500

    Tuesday night, initial express app

--- server/app.js
+++ server/app.js
@@ -1,19 +1,34 @@
-// 'Import' the http module
-const http = require("http");
-// Initialize the http server
-const server = http.createServer((request, response) => {
-    // Handle the request from http://localhost:4040/status with HTTP GET method
-    if (request.url === "/status" && request.method === "GET") {
-      // Create the headers for response
-      response.writeHead(200, { "Content-Type": "application/json" });
-      // Create the response body
-      response.write(JSON.stringify({ message: "Service Healthy" }));
-      // End and return the response
-      response.end();
-    }
-  })
-  // Tell the HTTP server to start listening
-  .listen(4040);
+// 'Import' the Express module instead of http
+const express = require("express");
+const dotenv = require("dotenv");
+// Initialize the Express application
+const app = express();
 
+dotenv.config();
+
+const PORT = process.env.API_PORT || 4040; // we use || to provide a default value
+
+const logging = (request, response, next) => {
+  console.log(`${request.method} ${request.url} ${Date.now()}`);
+  next();
+};
+
+app.use(express.json());
+app.use(logging);
+
+
+// Handle the request with HTTP GET method from http://localhost:4040/status
+app.get("/status", (request, response) => {
+   // Create the headers for response by default 200
+   // Create the response body
+   // End and return the response
+  response.status(418).json({ message: "Service healthy" });
+});
+
+app.post("/echo", (request, response) => {
+  response.json({ "request.body": request.body });
+});
+
+// Tell the Express app to start listening
 // Let the humans know I am running and listening on 4040
-console.log("Listening on port 4040");
+app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
